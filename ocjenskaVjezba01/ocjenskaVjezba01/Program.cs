3071d507f6f45c1ea5f1a61343989d35375e73a4
DavidCuturic5 <david.cuturic5@gmail.com>
2024-10-28 13:27:36 +0100

    Add files via upload

---
+++ ocjenskaVjezba01/ocjenskaVjezba01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ocjenskaVjezba01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite znakove: ");
+            string s1 = Console.ReadLine();
+
+            if (s1.Length < 5)
+            {
+                Console.WriteLine(s1);
+            }
+            else
+            {
+                string s2 = s1.Substring(2, s1.Length - 4);
+                Console.WriteLine(s2);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
